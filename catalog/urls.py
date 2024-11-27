61340c02f8370c1a5252ee34cf3726122ee4443f
Natasha Krasheninnikova <nk110905@mail.ru>
2024-11-28 00:05:42 +0700

    templates commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('account/', views.indexacc, name='profile'),
+    path('signup/', views.signup, name='signup'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,34 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .forms import *
 
 def index(request):
     return render(request, 'index.html')
-# Create your views here.
+
+@login_required
+def indexacc(request):
+    user_requests = Request.objects.filter(user=request.user)
+    return render(request, 'profile.html', {'user_requests': user_requests})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.fio = form.cleaned_data['fio']  # Предполагается, что это поле есть в форме
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user.set_password(password)
+            user.save()
+            user = authenticate(username=username, password=password)
+            if user is not None and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/')
+    else:
+        form = SignUpForm()  # Создаем форму для GET-запроса
+
+    return render(request, 'signup.html', {'form': form})  # Возвращаем форму для отображения
+
